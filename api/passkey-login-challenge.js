c2e0ebfa897b333a2080f4a349ec60cf9fe43649
matthewcarter95 <matthew.keith.carter@gmail.com>
2025-12-08 13:49:42 -0500

    changed auth0 domains to custom in code, but may have introduced new API JS

---
+++ api/passkey-login-challenge.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const response = await axios.post(`https://${process.env.AUTH0_DOMAIN}/api/v1/passkeys/login/challenge`, req.body, {
+      headers: { 'Content-Type': 'application/json' }
+    });
+    
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json(error.response?.data || { error: 'Request failed' });
+  }
+};
