f9f3ec9f2c794eedf00065f4eea8281a014191f7
matthewcarter95 <matthew.keith.carter@gmail.com>
2025-12-11 16:44:38 -0500

    Add req body to register2

--- api/passkey-signup.js
+++ api/passkey-signup.js
@@ -6,25 +6,7 @@ module.exports = async (req, res) => {
   }
 
   try {
-    const user_profile = {
-      
-      email: req.body.email
-      // name: req.body.name,
-      // Add other user profile fields as needed
-    };
-
-    const requestBody = {
-      // The realm where the passkey will be registered (e.g., 'Username-Password-Authentication')
-      realm: 'DefaultTenantPasskey', 
-      
-      // The profile information for the user (often includes 'user_id' and 'email')
-      user_profile: user_profile, 
-      
-      // (Optional) You can spread existing data from req.body if it contains other necessary fields
-      // ...req.body,
-    };
-    const response = await axios.post(`https://${process.env.AUTH0_DOMAIN}/passkey/register`, 
-      req.body, {
+    const response = await axios.post(`https://${process.env.AUTH0_DOMAIN}/passkey/register`, req.body, {
       headers: { 'Content-Type': 'application/json' }
     });
     
